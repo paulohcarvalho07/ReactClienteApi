e0113fcfedd3a85ba2ef4825372fe73a07106cab
paulohcarvalho07 <169151462+paulohcarvalho07@users.noreply.github.com>
2025-09-18 10:59:15 -0300

    Criação da página de Alunos

---
+++ src/pages/Alunos/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import './styles.css';
+
+import logoCadastro from '../../assets/cadastro1.png';
+
+import { FiXCircle, FiEdit, FiUserX } from 'react-icons/fi';
+
+export default function Alunos(){
+    return(
+        <div className="aluno-container">
+            <header>
+                <img src={logoCadastro} alt="Cadastro" />
+                <span>Bem vindo, <strong>Paulo</strong></span>
+                <Link className="button" to="aluno/novo">Novo Aluno</Link>
+                <button type="button">
+                    <FiXCircle size={35} color="#17202a"/>
+                </button>
+            </header>
+            <form>
+                <input type="text" placeholder="nome"/>
+                <button type="button" class="button">
+                    Filtrar aluno por nome (parcial)
+                </button>
+            </form>
+            <h1>Relação de Alunos</h1>
+            <ul>
+                <li>
+                <b>Nome:</b>Paulo<br/><br/>
+                <b>Email:</b>paulo@email.com<br/><br/>
+                <b>Idade:</b>22<br/><br/>    
+                <button type="button">
+                    <FiEdit size="25" color="#17202a" />
+                </button>           
+                <button type="button">
+                    <FiUserX size="25" color="#17202a" />
+                </button>         
+                </li>
+            </ul>
+        </div>
+    )
+}
---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Login from './pages/Login';
+import Alunos from './pages/Alunos';
+
+export default function Routes(){
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Login}/>
+                <Route path="/" component={Alunos}/>
+            </Switch>
+        </BrowserRouter>
+    )
+}
