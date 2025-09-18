16b0a2b2fb3888b5b59967dda76571bb13c4f7fe
paulohcarvalho07 <169151462+paulohcarvalho07@users.noreply.github.com>
2025-09-18 15:48:32 -0300

    Integração do React com a Api e ajustando o Login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,16 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.css';
 import logoImage from '../../assets/login.png';
+import api from '../../services/api';
+import { useHistory } from 'react-router-dom';
 
-export default function Login(){
+export default function Login() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const history = useHistory();
+
+    async function Login(event) {
+        event.preventDefault();
+
+        const data = {
+            email, password
+        };
+
+        try {
+
+            const response = await api.post('api/account/loginuser', data);
+
+            localStorage.setItem('email', email);
+            localStorage.setItem('token', response.data.token);
+            localStorage.setItem('expiration', response.data.expiration);
+
+            history.push('/alunos');
+        } catch (error) {
+            alert('O login falhou ' + error)
+        }
+    }
+
+
+
     return (
         <div className='login-container'>
             <section className='form'>
                 <img src={logoImage} alt='Login' id='img1' />
-                <form>
+                <form onSubmit={Login}>
                     <h1>Cadastro de Alunos</h1>
-                    <input placeholder='Email' />
-                    <input type='password' placeholder='Password'/>
+
+                    <input placeholder="Email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                    <input type="password" placeholder="Password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
+
                     <button class='button' type='submit'>Login</button>
                 </form>
             </section>
