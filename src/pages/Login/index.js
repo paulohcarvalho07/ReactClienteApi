47dd3a73918f3bd502eb8fe691123b8c632c61c9
paulohcarvalho07 <169151462+paulohcarvalho07@users.noreply.github.com>
2025-09-18 10:54:01 -0300

    Criando a aplicação React clientereact

---
+++ src/pages/Login/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './styles.css';
+import logoImage from '../../assets/login.png';
+
+export default function Login(){
+    return (
+        <div className='login-container'>
+            <section className='form'>
+                <img src={logoImage} alt='Login' id='img1' />
+                <form>
+                    <h1>Cadastro de Alunos</h1>
+                    <input placeholder='Email' />
+                    <input type='password' placeholder='Password'/>
+                    <button class='button' type='submit'>Login</button>
+                </form>
+            </section>
+        </div>
+    )
+}
